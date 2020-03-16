29ac035ea90b9bb2189a7127702cc1d764beac12
ruben <rubengargiulo@yahoo.it>
2020-03-16 17:01:56 +0100

    16/03/2020_17:01:56

--- django_site/urls.py
+++ django_site/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name='index.html'))
+    path("", login_required(TemplateView.as_view(template_name='index.html'))),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+
